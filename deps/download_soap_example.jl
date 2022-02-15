ada684d0e89ea10c760adc99ccb9209691df9539
Michael Palumbo <palumbomi95@gmail.com>
2022-02-15 11:14:56 -0500

    zenodo url

--- deps/download_soap_example.jl
+++ deps/download_soap_example.jl
@@ -3,8 +3,7 @@
 
 include("download.jl")
 
-# download_url = "https://pennstateoffice365-my.sharepoint.com/:u:/g/personal/ebf11_psu_edu/EYyYhOHEjM5RqvZn1D-4I58BDbpbcYg6QZKhd0KQ3N7YoQ?e=Is4PcG"
-download_url = "https://pennstateoffice365-my.sharepoint.com/:u:/g/personal/ebf11_psu_edu/EYyYhOHEjM5RqvZn1D-4I58BDbpbcYg6QZKhd0KQ3N7YoQ?download=1"
+download_url = "https://zenodo.org/record/6077993/files/soap_demo.h5?download=1"
 download_filename = joinpath("..","data","spectra","soap_demo.h5")
 download_md5 = "18a6291039d7684ef9187762e4b2dfd5"
 
