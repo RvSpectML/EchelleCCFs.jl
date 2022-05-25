a971626dbbdf4d7aa5b2568aa7b5ccbdfecf59bf
Matthias Yang He <myh7@psu.edu>
2022-05-24 22:13:07 -0400

    fix typos and indentation

--- src/calc_rv/fit_template_plus_deriv_to_ccf.jl
+++ src/calc_rv/fit_template_plus_deriv_to_ccf.jl
@@ -36,7 +36,7 @@ Construct functor to estimate RV based on the CCF.
 TODO: Implement correctly.  Not yet working/tested.
 Optional Arguments:
 - `v_grid`:  list of velocities where template is evaluated
-- `tempalte`: template ccf evaluated at v_grid
+- `template`: template ccf evaluated at v_grid
 - `frac_of_width_to_fit`: (0.5)
 - `measure_width_at_frac_depth`: (0.5)
 """
@@ -44,16 +44,16 @@ function MeasureRvFromCCFTemplate(; v_grid::AbstractVector{T1},
                                     template::AbstractVector{T2},
                                     frac_of_width_to_fit::Real = default_frac_of_width_to_fit,
                                     measure_width_at_frac_depth::Real = default_measure_width_at_frac_depth,
-									mean_var = 0
+				    mean_var = 0
                                      ) where { T1<:Real, T2<:Real }
     @assert length(v_grid) == length(template)
     @assert length(v_grid) >= 3
     @assert 0.25 <= measure_width_at_frac_depth <= 0.75
     @assert 0.1 <= frac_of_width_to_fit <= 5.0  # TODO: FIgure out appropriate range
-	@assert 0 <= mean_var < Inf
-	# Estimate derivative
+    @assert 0 <= mean_var < Inf
+    # Estimate derivative
     deriv = numerical_deriv(v_grid, template)
-	# find the min and fit only the part near the minimum of the CCF
+    # find the min and fit only the part near the minimum of the CCF
     v_min, v_idx_to_fit = find_idx_at_and_around_minimum(v_grid, template, frac_of_width_to_fit=frac_of_width_to_fit, measure_width_at_frac_depth=measure_width_at_frac_depth)
     if isnan(v_min) || isnothing(first(v_idx_to_fit)) || isnothing(last(v_idx_to_fit))
        v_idx_to_fit = 0:0
