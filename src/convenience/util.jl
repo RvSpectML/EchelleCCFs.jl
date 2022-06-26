74aae85eda166e1b34ec4047b874b93b7f0c64ea
Eric Ford <ebf11@psu.edu>
2022-06-25 22:31:31 -0400

    Fix #77

--- src/convenience/util.jl
+++ src/convenience/util.jl
@@ -1,13 +1,13 @@
 #using NamedTupleTools
 
 function add_tuple_sum(x::NT, y::NT) where { NT<:NamedTuple }
-    @assert all(fieldtypes(x) .== fieldtypes(y))
-    names_of_values = propertynames(x)
-    @assert propertynames(y)  == names_of_values
-    values_of_x = fieldvalues(x)
-    values_of_y = fieldvalues(y)
+    @assert all(typeof(x).types .== typeof(y).types)
+    names_of_values = keys(x)
+    @assert keys(y)  == names_of_values
+    values_of_x = values(x)
+    values_of_y = values(y)
     #nt = namedtuple(names_of_values, values_of_x .+ values_of_y )
     values = map(t->t[1].+t[2], zip(values_of_x,values_of_y))
-    nt = namedtuple(names_of_values, values )
+    nt = NamedTuple{names_of_values}( values )
     return nt
 end
